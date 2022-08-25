fe66ef9891e9dcd50ade50d3e8d0cdbbaa910dad
victor mathias <victormathias001@gmail.com>
2022-08-25 14:08:18 +0100

    Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div>
+      <div class="container">
+        <nav>Navbar</nav>
+        <main>Main</main>
+        <div id="sidebar">Sidebar</div>
+        <div id="content1">Content1</div>
+        <div id="content2">Content2</div>
+        <div id="content3">Content3</div>
+        <footer>Footer</footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
